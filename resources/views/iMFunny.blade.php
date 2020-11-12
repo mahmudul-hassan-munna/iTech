b1f2cf0699eac10f3d0c897b2bad77f332ed11cf
unknown <md.munnapust@gmail.com>
2020-11-12 22:04:24 +0600

    6(b) answer - done

---
+++ resources/views/iMFunny.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		.center {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h2 class="center">Answer to the question no 6(A):</h2>
+
+	<h2 class="center">Answer to the question no 6(B):</h2>
+	<p>There are 3 persons and 3 fishes. So, each person will get 1 piece of fish.</p>
+	Note: My Father, myself and my son go for fishing. 
+
+</body>
+</html>
