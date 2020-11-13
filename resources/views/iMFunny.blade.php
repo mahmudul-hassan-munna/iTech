02af5ec8aad4fc06dba379f3ca971c952f0a9b34
unknown <md.munnapust@gmail.com>
2020-11-13 18:13:17 +0600

    6 no qustion answered

--- resources/views/iMFunny.blade.php
+++ resources/views/iMFunny.blade.php
@@ -11,6 +11,26 @@
 <body>
 <h2 class="center">Answer to the question no 6(A):</h2>
 
+<h4 class="center"><u>My Opinion About Software Development of Bangladesh</u></h4>
+<p>The sector of software development in Bangladesh is not established yet, it is growing.</p>
+<p>In developed countries, software development is a hot cake now a days. The proper plan of
+    project development is root cause of it's success.</p>
+There are lot of things need to do for improving ourselves as a giant digital developed nation. Some
+of these are given below:
+
+<ul>
+    <li>Proper education</li>
+    <li>Implements of education</li>
+    <li>Research</li>
+    <li>Coding competition</li>
+    <li>Training</li>
+    <li>Seminar and discussion</li>
+    <li>International working environment</li>
+    <li>International level of salary</li>
+    <li>Job security for employee</li>
+    <li>Easy loan facilities for employer</li>
+</ul>
+
 <h2 class="center">Answer to the question no 6(B):</h2>
 <p>There are 3 persons and 3 fishes. So, each person will get 1 piece of fish.</p>
 Note: My Father, myself and my son go for fishing.
